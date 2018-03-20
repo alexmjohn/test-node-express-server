081aa82159c62a842734f0be18f2be97470a2be2
Alex John <alexmjohn01@gmail.com>
2018-03-20 23:37:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+})
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+  var now =new Date().toString();
+  console.log(`${now}`);
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render("maintenance.hbs");
+});
+
+app.get("/",(request,response)=>{
+  response.render("home.hbs",{
+    pageTitle:"Help",
+    currentYear:new Date().getFullYear(),
+    message:"some message"
+  });
+});
+
+app.get("/help",(request,response)=>{
+  response.render("help.hbs",{
+    pageTitle:"Help",
+    currentYear:new Date().getFullYear()
+  });
+});
+
+app.listen(3000,()=>console.log('server is up on port 3000'));
